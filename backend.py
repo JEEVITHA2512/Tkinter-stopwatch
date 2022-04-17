d15680e55b8114dbf03a480aeacd8a01e91d1a75
Jeevitha <jeevithamurugan.2512@gmail.com>
2022-04-17 13:35:02 +0530

    code updated successfully

--- backend.py
+++ backend.py
@@ -1,11 +1,23 @@
+from flask import Flask
+from tkinter import *
+app = Flask(__name__)
 
-from flask import Flask,render_template
+@app.route("/")
+def hello():
 
+    root = Tk()
 
-app=Flask(__name__)
+    out = Label(root, text="0", bg="purple")
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-if __name__ == '__main__':    
-    app.run(debug=True) 
+    def out_result():
+        out.configure(text="button pressed")
+
+    button1 = Checkbutton(root, command=out_result)
+
+    button1.place(x=20, y=20)
+    out.place(x=50, y=20)
+
+    root.mainloop()
+
+    if__name__= "__main__"
+    app.run()
